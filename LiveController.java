cff788f0c6be322f6d6497115c057179e5cc627d
Tsuchiya Kanm <150462533+kttsu@users.noreply.github.com>
2024-05-25 18:12:23 +0900

    Update LiveController.java
    インデントの修正を行いました。

--- LiveController.java
+++ LiveController.java
@@ -9,18 +9,19 @@ import java.net.URI;
 
 @RestController
 public class LiveController {
-   private final LiveService liveService;
-   public LiveController(LiveService liveService) {
-       this.liveService = liveService;
-   }
+    private final LiveService liveService;
+
+    public LiveController(LiveService liveService) {
+        this.liveService = liveService;
+    }
 
 
-@PostMapping("/live")
-public ResponseEntity<LiveResponse> insert(@RequestBody LiveRequest liveRequest, UriComponentsBuilder uriBuilder) {
-    Live live = liveService.insert(liveRequest.getSchedule(), liveRequest.getName(), liveRequest.getLocation());
-    URI location = uriBuilder.path("/live/{id}").buildAndExpand(live.getId()).toUri();
-    LiveResponse body = new LiveResponse("live created");
-    return ResponseEntity.created(location).body(body);
-   }
+    @PostMapping("/live")
+    public ResponseEntity<LiveResponse> insert(@RequestBody LiveRequest liveRequest, UriComponentsBuilder uriBuilder) {
+        Live live = liveService.insert(liveRequest.getSchedule(), liveRequest.getName(), liveRequest.getLocation());
+        URI location = uriBuilder.path("/live/{id}").buildAndExpand(live.getId()).toUri();
+        LiveResponse body = new LiveResponse("live created");
+        return ResponseEntity.created(location).body(body);
+    }
 }
 
