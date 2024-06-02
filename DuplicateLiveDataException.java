be349ad8e03ef875c42694ea075863c86e9771e4
Tsuchiya Kanm <150462533+kttsu@users.noreply.github.com>
2024-06-02 15:50:43 +0900

    SameLiveDataException.java→DuplicateLiveDataException.java に変更

--- DuplicateLiveDataException.java
+++ DuplicateLiveDataException.java
@@ -1,8 +1,8 @@
 package com.tsuchiya.live;
 
  // 既存のレコードと同じライブ情報を更新しようとした場合にスローするための例外処理クラス。
-public class SameLiveDataException extends RuntimeException {
-    public SameLiveDataException(String message) {
+public class DuplicateLiveDataException extends RuntimeException {
+    public DuplicateLiveDataException(String message) {
         super(message);
     }
 }
